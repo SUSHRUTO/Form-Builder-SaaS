5c1b7c45cacd3876d303fe5885ea72a997c9bc16
Sushruto Majumdar <sushruto26@gmail.com>
2026-05-27 22:47:04 +0530

    Fix production build and deployment issues

--- apps/web/providers/global.tsx
+++ apps/web/providers/global.tsx
@@ -1,41 +1,49 @@
 "use client";
 
+import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import React, { useState } from "react";
 import { Toaster } from "~/components/ui/sonner";
 
 import { trpc } from "~/trpc/client";
 import { createTRPCHttpBatchClientClient } from "~/trpc/create-client";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnMount: true,
-      staleTime: Infinity,
-    },
-  },
-});
+export const GlobalProviders = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnMount: true,
+            staleTime: Infinity,
+          },
+        },
+      }),
+  );
 
-export const GlobalProviders: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [createTRPCHttpBatchClientClient()],
     }),
   );
+
   return (
     <QueryClientProvider client={queryClient}>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="light"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <trpc.Provider queryClient={queryClient} client={trpcClient}>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
           <Toaster />
-        </trpc.Provider>
-      </NextThemesProvider>
+        </NextThemesProvider>
+      </trpc.Provider>
     </QueryClientProvider>
   );
 };
